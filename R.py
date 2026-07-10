de36eea6df3f3d2b914bbd5f5207d9457c98d309
AndreaCarolinaRamirezM <andreita.ramirez0171@gmail.com>
2026-07-09 21:53:23 -0500

    Feat:ManejoArchivoDeLectura

--- R.py
+++ R.py
@@ -1,6 +1,10 @@
 import random
 NOMBREARCHIVO="PalabrasIngles.txt"
-with open(NOMBREARCHIVO, "r") as Archivo:
-    for Linea in Archivo:
-        Palabra = Linea.strip()
-        print(Palabra)
+def Cargar_Archivo():
+    with open(NOMBREARCHIVO, "r", encoding="utf-8") as Archivo:
+        return [Linea.strip().upper() for Linea in Archivo]
+def Obtener_Palabra_Aleatoria(Palabras):
+    return random.choice(Palabras)
+Total_Palabras=Cargar_Archivo()
+Palabra_Aleatoria=Obtener_Palabra_Aleatoria(Total_Palabras)
+print(Palabra_Aleatoria)
